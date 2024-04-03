133cd06dc5024bae2750860b60765e0cc4c84dc1
IvunKup <ivkupel@gmail.com>
2024-04-03 19:42:46 +0300

    feat: added: ValueObject-FullName; Entity-Person;

---
+++ Domain/FullName.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+public class FullName
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FatherName { get; set;}
+}
---
+++ Domain/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain;
+
+public class BasePerson
+{
+    private Guid ID = Guid.NewGuid();
+}
+
+
+public class Person : BasePerson
+{
+    public FullName FullName { get; set; }
+    public string Gender { get; set; }
+    public DateTime Birthday { get; set; }
+    public string PhoneNumber { get; set; }
+    public string DiscordNic { get; set; }
+}
